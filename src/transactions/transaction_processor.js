c02b90a46b61e4ac7c751b1d8bab0e5df8a7c648
Debola Adedapo <niikee80@hotmail.com>
2024-01-31 16:56:00 +0000

    refactor: exercise refactor completed

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,5 +1,3 @@
-// var txr = [];
-
 function processTransactions(transActions) {
     let txr = []
 
@@ -7,21 +5,7 @@ function processTransactions(transActions) {
         throw new Error('Undefined collection of transactions')
     }
 
-    if (!transActions.length) return txr
-
-    if (!transActions.length === 1) return [`${transActions[0]} 1`]
-
-    // let txCount = {}
     let txCount = []
-
-    const numberOfTransactions = transActions.length
-
-    // for (var i = 0; i < numberOfTransactions; i++) {
-    //     const transaction = transActions[i]
-    //     txCount[transaction]
-    //         ? (txCount[transaction] += 1)
-    //         : (txCount[transaction] = 1)
-    // }
 
     transActions.forEach((item) => {
         const index = txCount.findIndex((x) => x.name === item)
@@ -30,58 +14,22 @@ function processTransactions(transActions) {
             : (txCount = [...txCount, { name: item, quantity: 1 }])
     })
 
-    console.log(txCount)
-    // txCount = sortByAmountThenName(txCount)
-    txCount.sort()
-
-    txCount.sort((itemOne, itemtwo) => {
-        return compareFn(itemOne, itemtwo)
-    })
-
-    // // Place them back in array for returning
-    // Object.keys(txCount).forEach(function (key, index) {
-    //     txr[index] = `${key} ${txCount[key]}`
-    // })
+    txCount.sort(compareFn)
 
     txr = txCount.map((cartItem) => `${cartItem.name} ${cartItem.quantity}`)
-    console.log(txr)
     return txr
 }
 
-// function sortByAmountThenName(txCount) {
-//     // let sortedKeys = Object.keys(txCount).sort(function sortingFunction(
-//     //     itemOne,
-//     //     itemTwo
-//     // ) {
-
-//     // })
-
-//     let sortedResults = {}
-//     for (let objectKey of sortedKeys) {
-//         sortedResults[objectKey] = txCount[objectKey]
-//     }
-
-//     return sortedResults
-// }
-
-function compareFn(itemOne, itemTwo, field) {
-    // return
-    // itemOne['quantity'] - itemTwo['quantity'] ||
-    //     itemOne['name'] > itemTwo['name'] ||
-    //     -(itemOne['name'] < itemTwo['name'])
-
-    return
-    itemOne.quantity - itemTwo.quantity
+function compareFn(itemOne, itemTwo) {
+    return (
+        itemTwo.quantity - itemOne.quantity ||
+        itemOne.name > itemTwo.name ||
+        -(itemOne.name < itemTwo.name)
+    )
 }
 
 function validateTransactions(transactions) {
-    // if (transactions === undefined) {
-    //     return false
-    // }
-
-    // return true
-    const result = transactions ? true : false
-    return result
+    return (result = transactions ? true : false)
 }
 
 module.exports = processTransactions
